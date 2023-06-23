d1fe797d791163798d9671d41c647b3a866bd27b
Musa Quaizar <132798005+MoosecianImmerse@users.noreply.github.com>
2023-06-23 07:58:42 -0500

    Update SetUp.py

--- SetUp.py
+++ SetUp.py
@@ -21,7 +21,17 @@ while run:
         white = "C:img/whiteman.png"
         brown = "C:img/brownman.png"
         browner = "C:img/brownerman.png"
+        palea = "C:img/palea.png"
+        whitea = "C:img/whitea.png"
+        browna = "C:img/browna.png"
+        brownera = "C:img/brownera.png"
+        paleb = "C:img/palemb.png"
+        whiteb = "C:img/whiteb.png"
+        brownb = "C:img/brownb.png"
+        brownerb = "C:img/brownerb.png"
         new_file_name = "img/guy.png"
+        new_file_name2 = "img/guyb.png"
+         new_file_name3 = "img/guya.png"
         
         (mouseX, mouseY) = pygame.mouse.get_pos()
         print (mouseX)
@@ -44,6 +54,8 @@ while run:
         
         if event.type == pygame.MOUSEBUTTONDOWN and 320>mouseX >0 and 160 >mouseY> 0:
             os.rename(white, new_file_name)
+            os.rename(whiteb, new_file_name2)
+            os.rename(whitea, new_file_name3)
             screen.blit(re_img, (0, 0))
             runn = True
             while runn:
@@ -53,6 +65,8 @@ while run:
                 pygame.display.update()
         if event.type == pygame.MOUSEBUTTONDOWN and 640>mouseX >321 and 160 >mouseY> 0:
             os.rename(browner, new_file_name)
+            os.rename(brownerb, new_file_name2)
+            os.rename(brownera, new_file_name3)
             screen.blit(re_img, (0, 0))
             runn = True
             while runn:
@@ -62,6 +76,8 @@ while run:
                 pygame.display.update()
         if event.type == pygame.MOUSEBUTTONDOWN and 320>mouseX >0 and 320 >mouseY> 161:
             os.rename(pale, new_file_name)
+            os.rename(paleb, new_file_name2)
+            os.rename(palea, new_file_name3)
             screen.blit(re_img, (0, 0))
             runn = True
             while runn:
@@ -71,6 +87,8 @@ while run:
                 pygame.display.update()
         if event.type == pygame.MOUSEBUTTONDOWN and 640>mouseX >321 and 320 >mouseY> 161:
             os.rename(brown, new_file_name)
+            os.rename(brownb, new_file_name2)
+            os.rename(browna, new_file_name3)
             screen.blit(re_img, (0, 0))
             runn = True
             while runn:
