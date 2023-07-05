1f95e39915298c928a6fa0d2f841c5eba9215258
Musa Quaizar <132798005+MoosecianImmerse@users.noreply.github.com>
2023-07-05 11:37:50 -0500

    Update Olympians Through Time.py

--- Olympians Through Time.py
+++ Olympians Through Time.py
@@ -318,7 +318,7 @@ class Player():
                         if engame_over == 0:
                                 draw_text('Take The L And Search Up A Walkthrough!', font_score, blue, (screen_width // 2)-450, screen_height // 2 + 100)
                         if engame_over == 1:
-                                draw_text('Take The L And Search Up A Walkthrough! Or Just Dont Kill Civilians...', font_score, blue, 0, screen_height // 2 + 100)
+                                draw_text('Take The L & Search Up A Walkthrough! Or Just Dont Kill', font_score, blue, 0, screen_height // 2 + 100)
                         if self.rect.y > 150:
                                 self.rect.y -= 5
                 
